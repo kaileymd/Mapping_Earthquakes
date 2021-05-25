1cc2f35497e69e9b2e7765453c11555483f9d0ef
Kailey Davis <kaileymichelledavis@gmail.com>
2021-05-25 15:58:59 -0500

    creating files to map single points

---
+++ Mapping_Single_Points/static/js/logic.js
@@ -0,0 +1,13 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+//tile layer
+let streets= L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: api_key
+});
+streets.addTo(map);

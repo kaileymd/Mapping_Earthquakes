3658a5c125e034c62be3a558d485bba33e84f736
Kailey Davis <kaileymichelledavis@gmail.com>
2021-05-25 16:18:58 -0500

    adding circle marker

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,12 +2,19 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 //tile layer
-let streets= L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets= L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: api_key
 });
 streets.addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437], {
+    color: 'black',
+    fillColor: 'yellow',
+    radius: 300
+ }).addTo(map);

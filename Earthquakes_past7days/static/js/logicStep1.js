46642f129bcc1068b659da52e1559e63b708246b
Kailey Davis <kaileymichelledavis@gmail.com>
2021-05-29 15:33:04 -0500

    adding equake points to map

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -22,8 +22,8 @@ let baseMaps = {
 
 // Create the map object with a center and zoom level.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
+  center: [39.5, -98.5],
+  zoom: 3,
   layers: [streets]
 });
 
@@ -32,18 +32,10 @@ L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
 // Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/kaileymd/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+let earthquakeJSON = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 // Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
+d3.json(earthquakeJSON).then(function(data) {
   console.log(data);
-  L.geoJSON(data, {
-    color: "blue",
-    fillColor: "yellow",
-    weight: 1,
-    onEachFeature: function(feature, layer) {
-      console.log(feature);
-      layer.bindPopup("<h3>" + feature.properties.AREA_NAME + "</h3>")
-    }
-  }).addTo(map);
+  L.geoJson(data).addTo(map);
 });
